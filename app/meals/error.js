1e935f0fd6781f512f0014825d77e0a9d44c97dd
istafamarshall <istafa.a.marshall@gmail.com>
2024-06-17 14:23:56 -0400

    create error handling files error.js and not-found.js

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An error occurried!</h1>
+            <p>Failed to fectch meal data</p>
+        </main>
+    )  
+}
