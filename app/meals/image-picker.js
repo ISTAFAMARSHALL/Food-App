11a70f91e368ea15bf5eb352d1360a2f06e38617
istafamarshall <istafa.a.marshall@gmail.com>
2024-06-18 00:22:54 -0400

    update image-picker.js

--- app/meals/image-picker.js
+++ app/meals/image-picker.js
@@ -1,15 +1,33 @@
+'use client'
+import { useRef } from 'react';
+
+import classes from './image-picker.module.css'
+
 export default function IamgePicker(props) {
+
+    const imageInput = useRef();
+
+    function handleIamgeClick () {
+        imageInput.current.click();
+    }
+
     return (
         <>
             <div className={classes.picker}>
                 <label htmlFor={props.name} >{props.label}</label>
                 <div className={classes.controls} >
-                    <input 
+                    <input
+                    className={classes.input}
                     type="file" 
                     id={props.name}
                     accept="image/png, image/jpeg" 
                     name={props.name}
+                    ref={imageInput}
                     />
+                    <button className={classes.button} 
+                    type='button'
+                    onClick={handleIamgeClick}
+                    >Pick an Image</button>
                 </div>
             </div>
         </>
