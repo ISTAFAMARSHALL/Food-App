02752edf5741f4c267eb0fad8a9d23d42045e01b
istafamarshall <istafa.a.marshall@gmail.com>
2024-06-17 23:44:04 -0400

    create not-found.js for meals slug

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,13 +1,41 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import classes from './page.module.css'
+import { getMeal } from '@/lib/meals'
+import { notFound } from 'next/navigation'
 
 export default function Plates (props) {
+
+    const meal = getMeal(props.params.slug)
+
+    if (!meal) {
+        notFound();
+    }
+    
+    let instructions = meal.instructions.replace(/\n/g, '<br />');
+
+
     return (
-        <main>
-            <h1>Plate</h1>
-            <p>{props.params.slug}</p>
-            <p>
-                <Link href='..' >Home Page</Link>
-            </p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.name} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={ `mailto:${meal.creator_email}` }>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+
+            <main>
+                <p className={classes.instructions}
+                    dangerouslySetInnerHTML={{__html: instructions}}
+                ></p>
+            </main>
+        </>
+        
     )
 }
---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className='not-found'>
+            <h1>Meals not Found</h1>
+            <p>Unfortunately, We cound not find the requested page or meal data</p>
+        </main>
+    )
+}
